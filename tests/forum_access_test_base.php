870ce86fe1003bb47258c598b7183d7bef012268
VasasA <vasasa@freemail.hu>
2019-11-21 18:26:23 +0100

    Fixed some bugs of self test

--- tests/forum_access_test_base.php
+++ tests/forum_access_test_base.php
@@ -156,7 +156,7 @@ class ForumAccessBaseTestCase extends ForumTestCase {
     $this->webmaster_rid = $this->backdropCreateRole(array('administer blocks', 'administer forums', 'administer nodes', 'administer comments', 'administer menu', 'administer taxonomy', 'create forum content', 'access content overview', 'access administration pages', 'view revisions', 'revert revisions', 'delete revisions'), '11 webmaster');
     $this->forum_admin_rid = $this->backdropCreateRole(array('administer forums', 'create forum content', 'edit any forum content', 'delete any forum content', /* 'access content overview', 'access administration pages', */), '12 forum admin');
     $this->edndel_any_content_rid = $this->backdropCreateRole(array('create forum content', 'edit any forum content', 'delete any forum content', 'view own unpublished content'), '13 edndel any content');
-    $this->edndel_own_content_rid = $this->backdropCreateRole(array('create forum content', 'edit own forum content', 'delete own forum content'), '14 edndel own content');
+    $this->edndel_own_content_rid = $this->backdropCreateRole(array('create forum content', 'edit own forum content', 'delete own forum content', 'edit own comments'), '14 edndel own content');
     $this->edit_any_content_rid = $this->backdropCreateRole(array('create forum content', 'edit any forum content', 'view own unpublished content'), '15 edit any content');
     $this->edit_own_content_rid = $this->backdropCreateRole(array('create forum content', 'edit own forum content', 'edit own comments'), '16 edit own content');
     $this->delete_any_content_rid = $this->backdropCreateRole(array('create forum content', 'delete any forum content', 'view own unpublished content'), '17 delete any content');
@@ -195,7 +195,7 @@ class ForumAccessBaseTestCase extends ForumTestCase {
       $this->delete_any_content_rid, $this->delete_own_content_rid, $this->create_content_rid,
     );
     // Show settings for reference.
-    $this->backdropGet('admin/people/permissions/list');
+    $this->backdropGet('admin/config/people/permissions');
     $this->assertResponse(200, '^^^ Permissions');
     $this->backdropGet('admin/people', array('query' => array('sort' => 'asc', 'order' => backdrop_encode_path(t('Username')))));
     $this->assertResponse(200, '^^^ Users');
